170d983e6760ae17c28b72f4c5254822eb797832
EC2 Default User <ec2-user@ip-172-31-8-207.ap-northeast-1.compute.internal>
2023-10-15 17:38:06 +0000

    [Update]課題2:アプリケーションを作成してみよう

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   get 'homes/show'
   get 'homes/edit'
   get 'books/new'
-  get 'books/:id' => 'books#show', as:'book'#入力データ
-  get '/show' => 'books#index'#入力データ一覧
+  get 'books/:id' => 'books#show', as: 'book'#入力データ
+  get '/books/show', to: 'books#index'#入力データ一覧
   get 'books/:id/edit' => 'books#edit',as: 'edit_book'#編集
+  patch 'books/:id' => 'books#update', as: 'update_book'#更新
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'#削除
   post '/books' => 'books#create'#データ保存
   get '/books' => 'books#new'#New Book
-  get '/top' => 'homes#top'#bookersへようこそ
+  get root to: 'homes#top'#bookersへようこそ
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
